64caca12db8920d5d3c086b97ba8921e0ed24241
pljenkins <pljenkins@gmail.com>
2013-08-08 21:35:08 -0700

    Initial cut at upgrading to ElasticSearch 0.90.3. Still need to verify test suite.

--- src/java/org/grails/plugins/elasticsearch/conversion/unmarshall/DomainClassUnmarshaller.java
+++ src/java/org/grails/plugins/elasticsearch/conversion/unmarshall/DomainClassUnmarshaller.java
@@ -178,7 +178,7 @@ public class DomainClassUnmarshaller {
                 return unmarshallReference(refDomainClass, data, unmarshallingContext);
             }
 
-            if (data.containsKey("class") && (Boolean)grailsApplication.getFlatConfig().get("elasticSearch.unmarshallComponents")) {
+            if (data.containsKey("class") && grailsApplication.getFlatConfig().get("elasticSearch.unmarshallComponents") != null && (Boolean)grailsApplication.getFlatConfig().get("elasticSearch.unmarshallComponents")) {
                 // Embedded instance.
                 if (!scpm.isComponent()) {
                     // maybe ignore?
@@ -253,7 +253,7 @@ public class DomainClassUnmarshaller {
                 .type(name)
                 .id(typeConverter.convertIfNecessary(id, String.class)))
                 .actionGet();
-        return unmarshallDomain(domainClass, response.id(), response.sourceAsMap(), unmarshallingContext);
+        return unmarshallDomain(domainClass, response.getId(), response.getSourceAsMap(), unmarshallingContext);
     }
 
 
